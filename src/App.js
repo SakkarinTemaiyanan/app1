7e7c5bc2da03f86b4c96b4072c8c72a4fa743d47
SakkarinTemaiyanan <s6403051633068@email.kmutnb.ac.th>
2024-08-11 21:03:18 +0700

    Function Component
    Function Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import './style.css'
-//import { Header,Content,Footer } from '.func-components'
+import { Header,Content,Footer } from './func-components'
 import Calendar from './class-components'
+
 
 function App(){
   return(
     <>
+      <Header/>
       <p><center><Calendar/></center></p>
+      <Content/>
+      <Footer/>
     </>
   )
+  
 }
 export default App;
