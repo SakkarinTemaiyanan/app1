2df7ac4f23b7c647edb7d2e1150bd73678df134f
SakkarinTemaiyanan <s6403051633068@email.kmutnb.ac.th>
2024-08-19 11:57:58 +0700

    RefsFunc
    RefsFunc

--- src/App.js
+++ src/App.js
@@ -2,33 +2,14 @@ import React from 'react'
 import Button  from './class-components'
 import { Calculator2 } from './calculator'
 import { EventData2 } from './event-data'
+import  RefsFunc   from './refs-func'
 
 export default function App() {
   return(
-    <>
-      <h2><center>react-class-comp-arrow-function</center></h2>
-      <Button/>
-      <h2><center>react-calculator</center></h2>
-      <Calculator2/>
-      <h2><center>react_event-data</center></h2>
-      <EventData2/>
-    </>
+  <>
+    <h2><center>react_refs-func</center></h2>
+    <RefsFunc/>
+  </>
+
   )
 }
-
-
-
-/*
-function App(){
-  return(
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
-  )
-  
-}
-export default App;
-*/
